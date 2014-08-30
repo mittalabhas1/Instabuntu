a2a52c2201e91f713edc0a063bf8335fbba87502
Abhas Mittal <mittalabhas1@gmail.com>
2014-08-30 16:19:01 +0530

    added google chrome
    From Google Linux repository on dl.google.com

--- index.sh
+++ index.sh
@@ -55,6 +55,11 @@ pip install virtualenv
 # VLC
 apt-get install vlc browser-plugin-vlc -y
 
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get install google-chrome-stable -y
+
 # Generating SSH Keys
 exit
 mkdir ~/.ssh
